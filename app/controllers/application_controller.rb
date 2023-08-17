195f75e8b200bb23566b69fa5b0380e45aac11f3
kitoken99 <kitoken99@gmail.com>
2023-08-17 13:10:31 +0900

    ユーザ認証にユーザ名の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_actions, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_actions
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
